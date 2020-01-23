f177db2be32302f9b3e35f8d982184c1b8a008a8
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 11:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function ssuperbowlWin(arr){
+  let search = arr.find(arr => arr.result === "O");
+
+  if (search === undefined){
+    return search;
+  }else{
+    return search.year;
+  }
+  return arr;
+}
